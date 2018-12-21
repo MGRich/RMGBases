0381a84a4715ea3e65aa111ad8a0c9274f6ec04a
MGRich <rmgrichdeveloper@gmail.com>
2018-12-21 12:26:40 -0600

    AAAAAAAAAAAAAAAAAAAAAAA e.

--- RMGLibrary/Changelog.cs
+++ RMGLibrary/Changelog.cs
@@ -11,25 +11,44 @@ namespace RMGLibrary
 {
     public class Changelog
     {
-        public static string[][] getData(string r, int history = 1)
+        public static string[][] getData(string r, int history)
         {
             if (Internet.isConnected() == false) return null;
-            if (history < 1) throw new ArgumentOutOfRangeException("history");
+            if (history < 0) throw new ArgumentOutOfRangeException("history");
             List<string[]> data = new List<string[]>();
             Internet.Download d = new Internet.Download();
-            JObject jdata = JObject.Parse(d.getString("https://api.github.com/repos/MGRich/" + r + "/releases"));
+            string s = d.getString("https://api.github.com/repos/MGRich/" + r + "/releases");
+            JArray jdata = JArray.Parse(s);
+            List<string> intl = new List<string>();
+            foreach (JObject o in jdata.Children<JObject>())
+            {
+                intl.Add(o.GetValue("id").ToString());
+                Console.WriteLine(o.GetValue("id"));
+            }
+            //intl.Reverse();
+            if (history == 0) history = intl.Count();
             for (int i = 0; i < history; i++)
             {
-                data.Add(getData(r, JObject.Parse(jdata[i].ToString()).GetValue("id").ToString()));
+                data.Add(getData(r, intl[i]));
             }
             return data.ToArray();
+        }
+
+        public static string[] getData(string r)
+        {
+            if (Internet.isConnected() == false) return null;
+            Internet.Download d = new Internet.Download();
+            string id = JObject.Parse(d.getString("https://api.github.com/repos/MGRich/" + r + "/releases/latest")).GetValue("id").ToString();
+            return getData(r, id);
         }
 
         private static string[] getData(string r, string id)
         {
             string[] data = new string[2];
             Internet.Download d = new Internet.Download();
-            JObject jdata = JObject.Parse(d.getString("https://api.github.com/repos/MGRich/" + r + "/releases/" + id));
+            string s = d.getString("https://api.github.com/repos/MGRich/" + r + "/releases/" + id);
+            //Console.WriteLine(s);
+            JObject jdata = JObject.Parse(s);
             data[0] = jdata.GetValue("name").ToString();
             data[1] = jdata.GetValue("body").ToString();
             return data;
